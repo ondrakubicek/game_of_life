53959fa7cdbe1e897b7b6f1b60124155fcf7296e
Ondra <ondys.kubicek@gmail.com>
2020-03-12 14:47:24 +0100

    output single

--- gameOfLifePhp/src/Grid.php
+++ gameOfLifePhp/src/Grid.php
@@ -2,59 +2,68 @@
 
 namespace GameOfLife;
 
-class Grid {
+class Grid
+{
 
-	/**
-	 * @var int
-	 */
-	private $width;
+    /**
+     * @var int
+     */
+    private $width;
 
-	/**
-	 * @var int
-	 */
-	private $height;
+    /**
+     * @var int
+     */
+    private $height;
 
-	/**
-	 * @var array
-	 */
-	public $cells = [];
+    /**
+     * @var array
+     */
+    public $cells = [];
 
-	public function __construct(int $width, int $height) {
-		$this->width = $width;
-		$this->height = $height;
-		$this->cells = array_fill(0, $height, array_fill(0, $width, 0));
-	}
+    public function __construct(int $width, int $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+        $this->cells = array_fill(0, $height, array_fill(0, $width, 0));
+    }
 
-	public function fillRandom() {
-		foreach ($this->cells as $x => $row) {
-			foreach ($row as $y => $cell) {
-				$this->cells[$x][$y] = rand(0, 1);
-			}
-		}
-	}
+    public function fillRandom(): void
+    {
+        foreach ($this->cells as $x => $row) {
+            foreach ($row as $y => $cell) {
+                $this->cells[$x][$y] = rand(0, 1);
+            }
+        }
+    }
 
-	public function clear() {
-		echo "\033[K";
-		echo "\033[0;0H";
-	}
+    public function clear(): void
+    {
+        echo "\033[K";
+        echo "\033[0;0H";
+    }
 
-	public function printGrid() {
-		foreach ($this->cells as $row) {
-			foreach ($row as $cell) {
-				echo $cell > 0 ? 0 : " ";
-			}
-			echo "\n";
-		}
-	}
+    public function printGrid(): void
+    {
+        $output = "";
+        foreach ($this->cells as $row) {
+            foreach ($row as $cell) {
+                $output .= $cell > 0 ? 0 : " ";
+            }
+            $output .= "\n";
+        }
+        echo $output;
+    }
 
-	public function getWidth(): int {
-		return $this->width;
-	}
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getHeight(): int {
-		return $this->height;
-	}
+    /**
+     * @return int
+     */
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
 }
